c03bafbc5389a5e1284fe92e6472cffd3f888911
Tadynas <griciustadas@gmail.com>
2022-09-26 12:42:54 +0300

    setup gulp functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var clean = require('gulp-clean');
 
 gulp.task('sass', function() {
     return gulp.src('src/styles/*.sass')
@@ -7,6 +8,12 @@ gulp.task('sass', function() {
             .pipe(gulp.dest('assets'))
 });
 
+gulp.task('clean', function(){
+    return gulp.src('assets/*.css', {read: false}).pipe(clean());
+});
+
 gulp.task('watch', function() {
     gulp.watch('src/styles/*.sass', gulp.series('sass'));
 })
+
+gulp.task('dev', gulp.series('clean', 'sass', 'watch'))
