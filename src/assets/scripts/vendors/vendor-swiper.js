22882831d5ae4e63d295e71a0ce81ada7334bc51
GretaVait895 <gretavait@gmail.com>
2022-10-21 15:55:17 +0300

    slideshow swiper added

--- src/assets/scripts/vendors/vendor-swiper.js
+++ src/assets/scripts/vendors/vendor-swiper.js
@@ -1,5 +1,12 @@
 const initSwiper = () => {
   console.log('Initialize Swiper!')
+  // const swiper = new Swiper('.slideshow__swiper', {
+  //   navigation: {
+  //     nextEl: '.slideshow__button-prev',
+  //     prevEl: '.slideshow__button-next',
+  //   },
+  // })
+  // console.log(swiper)
 }
 
-export default initSwiper
+initSwiper()
