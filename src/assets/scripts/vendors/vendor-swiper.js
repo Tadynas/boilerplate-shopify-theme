07f4a7411fb176215041caa3fc90bac862c132b3
GretaVait895 <gretavait@gmail.com>
2022-10-25 10:53:07 +0300

    slideshow section updated

--- src/assets/scripts/vendors/vendor-swiper.js
+++ src/assets/scripts/vendors/vendor-swiper.js
@@ -1,12 +1,5 @@
 const initSwiper = () => {
   console.log('Initialize Swiper!')
-  // const swiper = new Swiper('.slideshow__swiper', {
-  //   navigation: {
-  //     nextEl: '.slideshow__button-prev',
-  //     prevEl: '.slideshow__button-next',
-  //   },
-  // })
-  // console.log(swiper)
 }
 
 initSwiper()
